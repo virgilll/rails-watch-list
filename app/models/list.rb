aea2dd1472b8abce124fdc87142cbd06f9005084
Lucas Virgil <virgill@email.unc.edu>
2022-02-20 16:51:32 +0100

    watch list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy # if u destroy list, u destroy bookmarks
   has_many :movies, through: :bookmarks
   validates :name, presence: true
   validates :name, uniqueness: true
--- db/migrate/20220217105431_create_lists.rb
+++ db/migrate/20220217105431_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
       t.string :name
-
+      
       t.timestamps
     end
   end
