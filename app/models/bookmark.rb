61794ef8848f02e53de88ef06559b711d04b4d8f
Lucas Virgil <virgill@email.unc.edu>
2022-03-23 18:32:47 -0600

    movie api working

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,13 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, presence: true
-  validates :comment, length: { minimum: 6 }
+  # validates :comment, length: { minimum: 8 }
   validates :movie, uniqueness: { scope: :list }
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :title],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :lists, through: :bookmarks # basically a movie appears in many lists
   validates :title, presence: true
-  validates :title, uniqueness: true
+  # validates :title, uniqueness: true
   validates :overview, presence: true
 
   include PgSearch::Model
